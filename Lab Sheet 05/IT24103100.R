79b58f8c9449dbd9d01c1663bbe62cb4c2a151b6
IT24103100 <it24103100@my.sliit.lk>
2025-08-29 21:19:47 +0530

    Lab 5

---
+++ Lab Sheet 05/IT24103100.R
@@ -0,0 +1,32 @@
+setwd("C:\\Users\\daksh\\Desktop\\IT24103100")
+
+DeliveryTimes <- read.table("Exercise - Lab 05.txt", header = TRUE)
+View(DeliveryTimes)
+
+breaks <- seq(20, 70, length.out = 10)   
+
+hist(DeliveryTimes$Delivery_Time_.minutes., 
+     breaks = breaks, 
+     right = FALSE, 
+     col = "lightblue", 
+     main = "Histogram of Delivery Times", 
+     xlab = "Delivery Times (minutes)")
+
+
+
+breaks <- seq(20, 70, length.out = 10)
+
+hist_data <- hist(DeliveryTimes$Delivery_Time_.minutes., 
+                  breaks = breaks, 
+                  plot = FALSE, 
+                  right = FALSE)
+
+cum_freq <- cumsum(hist_data$counts)
+
+plot(hist_data$breaks[-1], cum_freq, type = "o", 
+     col = "blue", 
+     main = "Cumulative Frequency Polygon (Ogive)", 
+     xlab = "Delivery Times (minutes)", 
+     ylab = "Cumulative Frequency")
+
+
